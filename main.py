1fcbc2293798f1a1eaa5967b216d686f62cd9413
LuizDurand <luizfbd@al.insper.edu.br>
2021-05-11 17:25:23 -0300

    começo do código principal

---
+++ main.py
@@ -0,0 +1,19 @@
+#importa as funções
+import cria_baralho
+import Extrai_Naipe
+import extrai_valor
+import lista_movimento_possíveis
+import possui_movimentos_possiveis
+import empilha_cartas
+#começa jogo
+jogo = False
+
+jogador = input('Você quer jogar paciencia acordeão? ')
+if jogador == 'sim':
+    jogo = True
+else:
+    jogo = False
+
+if jogo == True:
+    while jogo:
+        
